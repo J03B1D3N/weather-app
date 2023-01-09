f23dc49baa0e5880ec15fad2e3cb903cbd3c81d0
Justas Lapiene <Justas63@gmail.com>
2023-01-09 16:09:15 +0100

    added restful api to render country abbreviations into full names.

--- src/script.js
+++ src/script.js
@@ -23,6 +23,12 @@ import "./style.scss";
       this.form = document.getElementById('form')
       this.api_key = '21ad911d5f1719a1d5ce294eec8a1017'
       },
+      getCountry: async function(c) {
+         const Countries = await fetch(`https://restcountries.com/v3.1/alpha/${c}`, {mode: 'cors'})
+         this.countriesJson = await Countries.json();
+         this.country = this.countriesJson[0].name.common
+         console.log(this.country)
+      } ,
       bindEvents: function() {
          form.addEventListener('submit', (e) => {
             e.preventDefault();
@@ -31,28 +37,29 @@ import "./style.scss";
       },
       getWeather: async function() {
          try {
-            const coordinates = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${this.input.value}&limit=1&appid=${this.api_key}`)
-            const city = await coordinates.json();
-            console.log(this.city)
-            const weatherJson = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${city[0].lat}&lon=${city[0].lon}&units=metric&appid=${this.api_key}`)
-            const weather = await weatherJson.json();
+            const GetCoordinates = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${this.input.value}&limit=1&appid=${this.api_key}`)
+            this.cityCoordinates = await GetCoordinates.json();
+            console.log(this.cityCoordinates)
+            const cityWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.cityCoordinates[0].lat}&lon=${this.cityCoordinates[0].lon}&units=metric&appid=${this.api_key}`)
+            this.weather = await cityWeather.json();
             
-            this.cityP.textContent = city[0].name
             console.log(this.weather)
-            this.currentTemp.textContent = 'Current temperature ' + weather.main.temp.toFixed(1) + '°C'
-            this.feelsLikeTemp.textContent = 'Feels like ' +weather.main.feels_like.toFixed(1) + '°C'
-            this. pressure.textContent = 'Atmospheric pressure ' + weather.main.pressure.toFixed(1)+ ' hPa'
-            this.maxTemp.textContent = 'Max temperature ' + weather.main.temp_max.toFixed(1) + '°C'
-            this.minTemp.textContent = 'Min temperature ' + weather.main.temp_min.toFixed(1) + '°C'
-         
-            this.weatherP.textContent = weather.weather[0].main
-            
-         
-         
-         
+            this.updateDom();
             } catch (err) {
                console.log(err)
             }
+         },
+         updateDom: function() {
+            this.getCountry(this.cityCoordinates[0].country)
+            this.cityP.textContent = this.cityCoordinates[0].name + ', ' + this.countriesJson[0].name.common
+            console.log()
+            this.currentTemp.textContent = 'Current temperature ' + this.weather.main.temp.toFixed(1) + '°C'
+            this.feelsLikeTemp.textContent = 'Feels like ' +this.weather.main.feels_like.toFixed(1) + '°C'
+            this. pressure.textContent = 'Atmospheric pressure ' + this.weather.main.pressure.toFixed(1)+ ' hPa'
+            this.maxTemp.textContent = 'Max temperature ' + this.weather.main.temp_max.toFixed(1) + '°C'
+            this.minTemp.textContent = 'Min temperature ' + this.weather.main.temp_min.toFixed(1) + '°C'
+         
+            this.weatherP.textContent = this.weather.weather[0].main
          }
       }
 
